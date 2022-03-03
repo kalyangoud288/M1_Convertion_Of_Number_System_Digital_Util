8f664be4c598c3beb2754ed79c34c9d0d38eb79c
kalyangoud288 <kalyangoud288@gmail.com>
2022-03-03 12:58:38 +0530

    commit

---
+++ 3_Implementation/func.h
@@ -0,0 +1,22 @@
+
+
+
+#ifndef FUNC__H__
+#define FUNC__H__
+#include <stdio.h>
+#include <math.h>
+#include<string.h>
+
+long int BinarytoDecimal(long int); //1:BINARY TO DECIMAL
+long int BinarytoOctal(long int); //2:BINARY TO OCTAL
+long int BinarytoHexa(long int); //3:BINARY TO HEXA-DECIMAL
+long int DecimaltoBinary(long int); //4:DECIMAL TO BINARY
+long int DecimaltoOctal(long int); //5:DECIMAL TO OCTAL
+long int DecimaltoHexa(long int); //6:DECIMAL TO HEXA-DECIMAL
+long int OctaltoBinary(long int); //7:OCTAL TO BINARY
+long int OctaltoDecimal(long int); //8:OCTAL TO DECIMAL
+long int OctaltoHexa(long int); //9:OCTAL TO HEXA-DECIMAL
+void Hexatobinary(char []); //10:HEXA-DECIMAL TO BINARY
+void HexatoDecimal(char []); //11:HEXA-DECIMAL TO DECIMAL
+void HexatoOctal(char []); //12:HEXA-DECIMAL TO OCTAL
+#endif
